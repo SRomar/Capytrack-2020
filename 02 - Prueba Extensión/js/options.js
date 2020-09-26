4a278d8c81751dd3a0f69f2c58b879fc7decbb1c
SRomar <sromar.contacto@gmail.com>
2020-09-26 18:10:29 -0300

    added interfaz opciones

--- 02 - Prueba Extensión/js/options.js
+++ 02 - Prueba Extensión/js/options.js
@@ -10,4 +10,75 @@ $(document).ready(function(){
     }
   });
 
+  
+  $(document).on('click','#btnSeguimientos', function() {
 
+    
+    $("#suscripcionesHTML").hide();
+    $("#h2SuscripcionesHTML").hide();
+    $("#configuracionContenedor").hide();
+    $("#contactoContenedor").hide();
+
+
+    $("#btnConfiguracion").css("background-color","#C4C4C4");
+    $("#btnSuscripciones").css("background-color","#C4C4C4");
+    $("#btnContacto").css("background-color","#C4C4C4");
+    $("#btnSeguimientos").css("background-color","#858585");
+
+
+    $("#seguimientosHTML").show();
+  });
+  
+  $(document).on('click','#btnSuscripciones', function() {
+    
+    $("#seguimientosHTML").hide();
+    $("#configuracionContenedor").hide();
+    $("#contactoContenedor").hide();
+
+
+    $("#btnSeguimientos").css("background-color","#C4C4C4")
+    $("#btnConfiguracion").css("background-color","#C4C4C4");
+    $("#btnContacto").css("background-color","#C4C4C4");
+    $("#btnSuscripciones").css("background-color","#858585");
+
+    $("#h2SuscripcionesHTML").show();
+    $("#suscripcionesHTML").show();
+  
+});
+
+$(document).on('click','#btnConfiguracion', function() {
+
+  $("#seguimientosHTML").hide();
+  $("#suscripcionesHTML").hide();
+  $("#h2SuscripcionesHTML").hide();
+  $("#contactoContenedor").hide();
+
+  $("#btnSeguimientos").css("background-color","#C4C4C4");
+  $("#btnContacto").css("background-color","#C4C4C4");
+  $("#btnSuscripciones").css("background-color","#C4C4C4");
+  $("#btnConfiguracion").css("background-color","#858585");
+
+
+  $("#configuracionContenedor").show();
+
+});
+  
+$(document).on('click','#btnContacto', function() {
+
+  $("#seguimientosHTML").hide();
+  $("#suscripcionesHTML").hide();
+  $("#h2SuscripcionesHTML").hide();
+  $("#configuracionContenedor").hide();
+
+
+  $("#btnSeguimientos").css("background-color","#C4C4C4");
+  $("#btnContacto").css("background-color","#858585");
+  $("#btnSuscripciones").css("background-color","#C4C4C4");
+  $("#btnConfiguracion").css("background-color","#C4C4C4");
+
+
+  $("#contactoContenedor").show();
+
+});
+  
+  
