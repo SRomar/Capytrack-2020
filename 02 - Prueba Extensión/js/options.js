16ae85943779a66290691c53bd9c9d46e72672fb
SRomar <sromar.contacto@gmail.com>
2020-10-12 18:40:23 -0300

    Elimine 'Options copy.js'

--- 02 - Prueba Extensión/js/options.js
+++ 02 - Prueba Extensión/js/options.js
@@ -1,106 +1,192 @@
-var itemLista;
-var itemProducto;
-var lista = false;
-var producto = false;
 var listaSeleccionada;
 
 $(document).ready(function(){
-    if($( "#listaDeListas" ).val() !== null){
-      chrome.storage.sync.get(null, function(items) {
-          var allKeys = Object.keys(items);
-          for (i = 0; i < allKeys.length; i++) {
 
-              //$("#listaDeListas").append('<li><a class="elementoLista" href="#">'+allKeys[i]+'</a></li>');  
-              $("#listaDeListas").append('<li class="elementoLista" id="'+allKeys[i]+'">'+allKeys[i]+'</li>');  
+  EventosBotones();
+  DesplegarListas();
+  EventoListas();
+  CrearContextMenu();
 
-          }
-         
-        document.querySelectorAll('.elementoLista').forEach(item => {
-          // item.addEventListener('click', event => {
-          $(item).on('click', function() {
-              
-              $('#productosUl').empty()
-                var nombreLista = $(this).text(); //Obtiene el nombre de li
-                listaSeleccionada = nombreLista;
-                chrome.storage.sync.get(nombreLista, function (lista) { //Obtiene la lista
-                
-                  $.map(lista, function(productosEnLista, nombreProducto) { //Obtiene los productos en la lista
-                
-                      $.map(productosEnLista, function(producto, llaveProducto) {  //Separa los productos
+});
+
+
+function EventosBotones(){
+  $(document).on('click','#btnSeguimientos', function() {
+
       
-                        $.map(producto, function(datosProducto, categoryID) { //Separa los datos del producto
-
-                          $("#productosUl").append('<li class="productoEnLista" id="'+datosProducto[5]+'">'+datosProducto[0]+'</li>'); //De aca se pueden sacar los datos del producto usando el indice
-                           
-                          var identificacion = "#"+datosProducto[5];
-
-                          $(identificacion).on('click', function() {
-  
-                            $('#contenedorImagen').empty()
-                      
+      $("#suscripcionesHTML").hide();
+      $("#h2SuscripcionesHTML").hide();
+      $("#configuracionContenedor").hide();
+      $("#contactoContenedor").hide();
+      $('#productosUl > li').remove();
 
 
-                            $("#contenedorImagen").append('<img src="'+datosProducto[4]+'" class="imagenProducto" alt="celular"></img>');
-                            nombre.innerHTML = datosProducto[0];
-                            estado.innerHTML = datosProducto[2];      
-                            precio.innerHTML = datosProducto[1];
-                            urlProducto.innerHTML = '<a href="'+datosProducto[3]+'">ver</a>';
-
-                          });
+      $("#btnConfiguracion").css("background-color","#C4C4C4");
+      $("#btnSuscripciones").css("background-color","#C4C4C4");
+      $("#btnContacto").css("background-color","#C4C4C4");
+      $("#btnSeguimientos").css("background-color","#858585");
 
 
-
-                    
-                  });
-                });
-              });
-            });
-          });   
-        });
-      }); 
-  
-    }
-
-  
-
-    
-    $("#menu").hide();
-
-    $(document).bind("contextmenu", function(e){
-      comprobacion();
-      return false;
+      $("#seguimientosHTML").show();
     });
     
-    function comprobacion(){
+  $(document).on('click','#btnSuscripciones', function() {
+      
+      $("#seguimientosHTML").hide();
+      $("#configuracionContenedor").hide();
+      $("#contactoContenedor").hide();
+      $('#productosUl > li').remove();
 
-      $("#listaDeListas").mousedown(function(e){
-        if(e.button == 2){
-          producto = false;
-          lista = true;
-          itemLista = $(e.target).text();
-          document.getElementById("cambiarNombreLista").style.display = "inline";
-          if(itemLista.toString() == ''){
-            lista = false;
-          }
-          $("#menu").css({'display':'block', 'left':e.pageX, 'top':e.pageY});
-        }
-      });
 
-      $("#productosUl").mousedown(function(e){
-        if(e.button == 2){
-          lista = false;
-          producto = true;
-          itemProducto = $(e.target).text();
-          document.getElementById("cambiarNombreLista").style.display = "none";
-          if(itemProducto.toString() == ''){
-            producto = false;
-          }
-          $("#menu").css({'display':'block', 'left':e.pageX, 'top':e.pageY});
-        }
-      });
-    }
+      $("#btnSeguimientos").css("background-color","#C4C4C4")
+      $("#btnConfiguracion").css("background-color","#C4C4C4");
+      $("#btnContacto").css("background-color","#C4C4C4");
+      $("#btnSuscripciones").css("background-color","#858585");
+
+      $("#h2SuscripcionesHTML").show();
+      $("#suscripcionesHTML").show();
     
-    $("#cambiarNombreLista").click(function(e){
+  });
+
+  $(document).on('click','#btnConfiguracion', function() {
+
+    $("#seguimientosHTML").hide();
+    $("#suscripcionesHTML").hide();
+    $("#h2SuscripcionesHTML").hide();
+    $("#contactoContenedor").hide();
+    $('#productosUl > li').remove();
+
+    $("#btnSeguimientos").css("background-color","#C4C4C4");
+    $("#btnContacto").css("background-color","#C4C4C4");
+    $("#btnSuscripciones").css("background-color","#C4C4C4");
+    $("#btnConfiguracion").css("background-color","#858585");
+
+
+    $("#configuracionContenedor").show();
+
+  });
+    
+  $(document).on('click','#btnContacto', function() {
+
+  $("#seguimientosHTML").hide();
+  $("#suscripcionesHTML").hide();
+  $("#h2SuscripcionesHTML").hide();
+  $("#configuracionContenedor").hide();
+  $('#productosUl > li').remove();
+
+
+  $("#btnSeguimientos").css("background-color","#C4C4C4");
+  $("#btnContacto").css("background-color","#858585");
+  $("#btnSuscripciones").css("background-color","#C4C4C4");
+  $("#btnConfiguracion").css("background-color","#C4C4C4");
+
+
+  $("#contactoContenedor").show();
+
+});
+}
+
+function DesplegarListas(){
+
+  if($( "#listasUl" ).val() !== null){
+    chrome.storage.sync.get(null, function(items) {
+        var allKeys = Object.keys(items);
+        for (i = 0; i < allKeys.length; i++) {
+
+            $("#listasUl").append('<li class="elementoLista" id="'+allKeys[i]+'">'+allKeys[i]+'</li>');  
+
+        }
+
+    });
+  } 
+}
+
+function EventoListas(){
+  chrome.storage.sync.get(null, function(items) {
+  document.querySelectorAll('.elementoLista').forEach(item => {
+
+    $(item).on('click', function() {      
+      $('#productosUl').empty()
+      var nombreLista = $(this).text(); //Obtiene el nombre de li
+      listaSeleccionada = nombreLista;
+      desplegarProductos(nombreLista);
+    });  
+  });
+  });
+}
+
+function desplegarProductos(nombreLista){
+  chrome.storage.sync.get(nombreLista, function (lista) { //Obtiene la lista
+
+    $.map(lista, function(productosEnLista, nombreProducto) { //Obtiene los productos en la lista
+  
+        $.map(productosEnLista, function(producto, llaveProducto) {  //Separa los productos
+
+          $.map(producto, function(datosProducto, categoryID) { //Separa los datos del producto
+
+            $("#productosUl").append('<li class="productoEnLista" id="'+datosProducto[5]+'">'+datosProducto[0]+'</li>'); //De aca se pueden sacar los datos del producto usando el indice
+        
+            var idProducto = "#"+datosProducto[5];
+            EventoProducto(idProducto, datosProducto);
+            
+        });
+      });
+    });
+  });
+}
+
+function EventoProducto(idProducto, datosProducto){
+      $(idProducto).on('click', function() {
+        $('#contenedorImagen').empty()
+        $("#contenedorImagen").append('<img src="'+datosProducto[4]+'" class="imagenProducto" alt="celular"></img>');
+        nombre.innerHTML = datosProducto[0];
+        estado.innerHTML = datosProducto[2];      
+        precio.innerHTML = datosProducto[1];
+        urlProducto.innerHTML = '<a href="'+datosProducto[3]+'">ver</a>';
+    });
+}
+
+function CrearContextMenu(){
+  $(document).bind("contextmenu", function(e){
+    eventoOcultarMenu();
+    comprobacion();
+    eventoCambiarNombreLista();
+    return false;
+  });
+}
+
+function comprobacion(){
+
+  $(".elementoLista").mousedown(function(e){
+    if(e.button == 2){
+      producto = false;
+      lista = true;
+      itemLista = $(e.target).text();
+      itemSeleccionado.innerHTML = " "+itemLista;
+      document.getElementById("cambiarNombreLista").style.display = "inline";
+      eventoEliminarLista(itemLista);
+      $("#menu").css({'display':'block', 'left':e.pageX, 'top':e.pageY});
+    }
+  });
+
+  $(".productoEnLista").mousedown(function(e){
+
+    if(e.button == 2){
+      lista = false;
+      producto = true;
+      itemProducto = $(e.target).text();
+      itemSeleccionado.innerHTML = " "+itemProducto;
+      document.getElementById("cambiarNombreLista").style.display = "none";
+      eventoEliminarProducto(itemProducto);
+
+      $("#menu").css({'display':'block', 'left':e.pageX, 'top':e.pageY});
+    }
+  });
+}
+
+function eventoCambiarNombreLista(){ 
+  $("#cambiarNombreLista").click(function(e){
+
       var resp = window.prompt("Nuevo nombre:");
       var existe = false;
       
@@ -144,145 +230,69 @@ $(document).ready(function(){
           }
         });
       }
-      
      
-      lista = false;
-      producto = false;
 
-    });
-
-    $("#eliminar").click(function(e){
-
-      if(lista == true){
-        chrome.storage.sync.remove(itemLista);
-      }
-      else if(producto == true){
-        var productosLista = [];
-        var listaNueva = {};
-        var existe = false;
+  });
+}
 
 
+function eventoEliminarProducto(itemProducto){
+  $("#eliminar").click(function(e){
+      var productosLista = [];
+      var listaNueva = {};
+      var existe = false;
 
-        chrome.storage.sync.get(listaSeleccionada, function (lista) { //Obtiene la lista
-            
-          $.map(lista, function(productosEnLista, listaSeleccionada) { //Obtiene los productos en la lista
+      chrome.storage.sync.get(listaSeleccionada, function (lista) { //Obtiene la lista
+          
+        $.map(lista, function(productosEnLista, listaSeleccionada) { //Obtiene los productos en la lista
+          
+          
+          $.map(productosEnLista, function(producto, llaveProducto) {  //Separa a los productos
             
             
-            $.map(productosEnLista, function(producto, llaveProducto) {  //Separa a los productos
-              
-              
-              $.map(producto, function(datosProducto, categoryID) { //Separa a los datos del producto
-                if(datosProducto[0] !== itemProducto){
-                  productosLista.push(producto);
-                }
-                else if(datosProducto[0] == itemProducto){
-                  existe = true;
-                }
-              });
+            $.map(producto, function(datosProducto, categoryID) { //Separa a los datos del producto
+              if(datosProducto[0] !== itemProducto){
+                productosLista.push(producto);
+              }
+              else if(datosProducto[0] == itemProducto){
+                existe = true;
+              }
             });
           });
-          if(existe == true){
-            listaNueva[listaSeleccionada] = productosLista;        
-            chrome.storage.sync.remove(listaSeleccionada);
-            chrome.storage.sync.set(listaNueva);
-          }
-          
         });
-
+        if(existe == true){
+          listaNueva[listaSeleccionada] = productosLista;        
+          chrome.storage.sync.remove(listaSeleccionada);
+          chrome.storage.sync.set(listaNueva);
+        }
         
-
-      }
-     location.reload();
-     
-      lista = false;
-      producto = false;
-    });
-
-
-    $(window).click(function() {
-      lista = false;
-      producto = false;
-      producto = false;
-     $("#menu").hide();
       });
-
-  });
-
-
-  $(document).on('click','#btnSeguimientos', function() {
-
+      
     
-    $("#suscripcionesHTML").hide();
-    $("#h2SuscripcionesHTML").hide();
-    $("#configuracionContenedor").hide();
-    $("#contactoContenedor").hide();
-    $('#productosUl > li').remove();
+      setTimeout(function (){
+        $('#productosUl').empty();
+        desplegarProductos(listaSeleccionada);
+      }, 100);
 
-
-    $("#btnConfiguracion").css("background-color","#C4C4C4");
-    $("#btnSuscripciones").css("background-color","#C4C4C4");
-    $("#btnContacto").css("background-color","#C4C4C4");
-    $("#btnSeguimientos").css("background-color","#858585");
-
-
-    $("#seguimientosHTML").show();
+    lista = false;
+    producto = false;
   });
-  
-  $(document).on('click','#btnSuscripciones', function() {
-    
-    $("#seguimientosHTML").hide();
-    $("#configuracionContenedor").hide();
-    $("#contactoContenedor").hide();
-    $('#productosUl > li').remove();
+}
+
+function eventoEliminarLista(itemLista){
+  $("#eliminar").click(function(e){
+      chrome.storage.sync.remove(itemLista);
+      $('#listasUl').empty();
+      DesplegarListas();
+  });
+}
 
 
-    $("#btnSeguimientos").css("background-color","#C4C4C4")
-    $("#btnConfiguracion").css("background-color","#C4C4C4");
-    $("#btnContacto").css("background-color","#C4C4C4");
-    $("#btnSuscripciones").css("background-color","#858585");
-
-    $("#h2SuscripcionesHTML").show();
-    $("#suscripcionesHTML").show();
-  
-});
-
-$(document).on('click','#btnConfiguracion', function() {
-
-  $("#seguimientosHTML").hide();
-  $("#suscripcionesHTML").hide();
-  $("#h2SuscripcionesHTML").hide();
-  $("#contactoContenedor").hide();
-  $('#productosUl > li').remove();
-
-  $("#btnSeguimientos").css("background-color","#C4C4C4");
-  $("#btnContacto").css("background-color","#C4C4C4");
-  $("#btnSuscripciones").css("background-color","#C4C4C4");
-  $("#btnConfiguracion").css("background-color","#858585");
-
-
-  $("#configuracionContenedor").show();
-
-});
-  
-$(document).on('click','#btnContacto', function() {
-
-  $("#seguimientosHTML").hide();
-  $("#suscripcionesHTML").hide();
-  $("#h2SuscripcionesHTML").hide();
-  $("#configuracionContenedor").hide();
-  $('#productosUl > li').remove();
-
-
-  $("#btnSeguimientos").css("background-color","#C4C4C4");
-  $("#btnContacto").css("background-color","#858585");
-  $("#btnSuscripciones").css("background-color","#C4C4C4");
-  $("#btnConfiguracion").css("background-color","#C4C4C4");
-
-
-  $("#contactoContenedor").show();
-
-});
-
-
-
-  
+function eventoOcultarMenu(){
+  $(window).click(function() {
+    lista = false;
+    producto = false;
+    producto = false;
+  $("#menu").hide();
+    });
+}
