d6688540792a0d70f21eb19e16b7232c51b3673c
SRomar <sromar.contacto@gmail.com>
2020-10-19 17:39:18 -0300

    Mejore la interfaz del context menu

--- 02 - Prueba Extensión/js/options.js
+++ 02 - Prueba Extensión/js/options.js
@@ -91,6 +91,7 @@ function CrearContextMenu(){
     EventoOcultarMenu();
     Comprobacion();
     EventoCambiarNombreLista();
+    EventoFondo();
     return false;
   });
 }
@@ -265,3 +266,17 @@ function EventoOcultarMenu(){
     });
 }
 
+function EventoFondo(){
+  $("#eliminar").mouseover(function(){
+    $("#eliminar").css("background-color", "#757575");
+    $("#cambiarNombreLista").css("background-color", "#ffffff");
+  });
+  $("#cambiarNombreLista").mouseover(function(){
+    $("#eliminar").css("background-color", "#ffffff");
+    $("#cambiarNombreLista").css("background-color", "#757575");
+  });
+  $(".contextMenu").mouseleave(function(){
+    $("#eliminar").css("background-color", "#ffffff");
+    $("#cambiarNombreLista").css("background-color", "#ffffff");
+  });
+}
